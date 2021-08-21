1f6a1579c8797ca7d9236b4148cd53d613de7b31
Andrea Siede <andrea.siede@gmail.com>
2021-08-21 16:43:46 -0700

    updates betEvent password to not required

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bcryptjs');
-const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
@@ -7,68 +7,80 @@ const betEventSchema = mongoose.Schema({
   name: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: false,
   },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }]
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  questions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Question" }],
 });
 
 const questionSchema = mongoose.Schema({
   questionId: { type: Number },
   text: {
     type: String,
-    required: true
+    required: true,
   },
-  answers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Answer' }]
+  answers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Answer" }],
 });
 
 const answerSchema = mongoose.Schema({
   answerId: { type: Number },
   text: {
     type: String,
-    required: true
+    required: true,
   },
   odds: {
-    type: Number
+    type: Number,
   },
   confirmed: {
-    type: Boolean
-  }
+    type: Boolean,
+  },
 });
 
 const submissionSchema = mongoose.Schema({
   bettor: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
-  bets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bet', required: true }],
-  betEvent: { type: mongoose.Schema.Types.ObjectId, ref: 'BetEvent', required: true }
+  bets: [{ type: mongoose.Schema.Types.ObjectId, ref: "Bet", required: true }],
+  betEvent: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "BetEvent",
+    required: true,
+  },
 });
 
 const betSchema = mongoose.Schema({
   questionId: { type: { Number } },
-  answerId: { type: { Number } }
+  answerId: { type: { Number } },
 });
 
 const userSchema = mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
-  email: { type: String, required: true, unique: true }
+  firstName: {
+    type: String,
+    required: false,
+  },
+  lastName: {
+    type: String,
+    required: false,
+  },
+  email: { type: String, required: true, unique: true },
 });
 
-userSchema.methods.serialize = function() {
+userSchema.methods.serialize = function () {
   return {
     id: this.id,
     // id: this._id,
@@ -77,23 +89,22 @@ userSchema.methods.serialize = function() {
     firstName: this.firstName,
     lastName: this.lastName,
     email: this.email,
-    trips: this.trips
   };
 };
 
-userSchema.methods.validatePassword = function(password) {
+userSchema.methods.validatePassword = function (password) {
   return bcrypt.compare(password, this.password);
 };
 
 userSchema.statics.hashPassword = (password) => bcrypt.hash(password, 10);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
-const BetEvent = mongoose.model('BetEvent', betEventSchema);
-const Submission = mongoose.model('Submission', submissionSchema);
-const Question = mongoose.model('Question', questionSchema);
-const Answer = mongoose.model('Answer', answerSchema);
-const Bet = mongoose.model('Bet', betSchema);
+const BetEvent = mongoose.model("BetEvent", betEventSchema);
+const Submission = mongoose.model("Submission", submissionSchema);
+const Question = mongoose.model("Question", questionSchema);
+const Answer = mongoose.model("Answer", answerSchema);
+const Bet = mongoose.model("Bet", betSchema);
 
 module.exports = {
   User,
@@ -101,5 +112,5 @@ module.exports = {
   Submission,
   Question,
   Answer,
-  Bet
+  Bet,
 };
